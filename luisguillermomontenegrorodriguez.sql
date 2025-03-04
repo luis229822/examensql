745d3228bb1038fa4b8cd16f67ace1540ef50cda
luis229822 <luis22199822@gmail.com>
2025-03-04 12:41:18 -0500

    Update luisguillermomontenegrorodriguez.sql

--- luisguillermomontenegrorodriguez.sql
+++ luisguillermomontenegrorodriguez.sql
@@ -71,27 +71,39 @@ VALUES ('Ronald', 'Caballero Sopan', '10105952023', 'av. pacifico 238');
 -- Insertar Vendedor
 INSERT INTO Vendedores (nombres, apellidos) VALUES 
 ('Bob', 'Esponja');
+INSERT INTO Vendedores (nombres, apellidos) VALUES 
 ('Gianina', 'leon Rojas');
+INSERT INTO Vendedores (nombres, apellidos) VALUES 
 ('laura', 'bozzo villegas');
+INSERT INTO Vendedores (nombres, apellidos) VALUES 
 ('Mario', ' vargas llosa');
 
 
---INSERT INTO Facturas (numero_factura, fecha, id_cliente, id_vendedor, condicion_pago, subtotal, igv, total) VALUES
-('F002-00000345', '2018-01-24', 1, 1, 'CONTADO C/E', 241.53, 43.47, 286.00),
-('F002-00000346', '2018-02-15', 2, 2, 'CRÉDITO', 320.00, 57.60, 377.60),
-('F002-00000347', '2018-03-10', 3, 3, 'CONTADO', 150.75, 27.13, 177.88),
+INSERT INTO Facturas (numero_factura, fecha, id_cliente, id_vendedor, condicion_pago, subtotal, igv, total) VALUES
+('F002-00000345', '2018-01-24', 1, 1, 'CONTADO C/E', 241.53, 43.47, 286.00);
+INSERT INTO Facturas (numero_factura, fecha, id_cliente, id_vendedor, condicion_pago, subtotal, igv, total) VALUES
+('F002-00000346', '2018-02-15', 2, 2, 'CRÉDITO', 320.00, 57.60, 377.60);
+INSERT INTO Facturas (numero_factura, fecha, id_cliente, id_vendedor, condicion_pago, subtotal, igv, total) VALUES
+('F002-00000347', '2018-03-10', 3, 3, 'CONTADO', 150.75, 27.13, 177.88);
+INSERT INTO Facturas (numero_factura, fecha, id_cliente, id_vendedor, condicion_pago, subtotal, igv, total) VALUES
 ('F002-00000348', '2018-04-05', 4, 4, 'CRÉDITO', 500.00, 90.00, 590.00);
 
 -- Insertar Productos
 INSERT INTO Productos (codigo, descripcion, precio_unitario) VALUES
-('DRI000012', 'DRILL 2B01 1RA GOLDEN POL', 8.0508),
-('TEX000024', 'TELA JEAN AZUL OSCURO', 12.5000),
-('ALG000078', 'ALGODÓN PREMIUM BLANCO', 7.2500),
+('DRI000012', 'DRILL 2B01 1RA GOLDEN POL', 8.0508);
+INSERT INTO Productos (codigo, descripcion, precio_unitario) VALUES
+('TEX000024', 'TELA JEAN AZUL OSCURO', 12.5000);
+INSERT INTO Productos (codigo, descripcion, precio_unitario) VALUES
+('ALG000078', 'ALGODÓN PREMIUM BLANCO', 7.2500);
+INSERT INTO Productos (codigo, descripcion, precio_unitario) VALUES
 ('POLY000056', 'POLIÉSTER RESISTENTE NEGRO', 9.7500);
 
 -- Insertar Detalle de Factura
 INSERT INTO Detalle_Factura (id_factura, id_producto, cantidad, total) VALUES
-(1, 1, 30.00, 241.53),
-(2, 2, 25.00, 320.00),
-(3, 3, 20.80, 150.75),
+(1, 1, 30.00, 241.53);
+INSERT INTO Detalle_Factura (id_factura, id_producto, cantidad, total) VALUES
+(2, 2, 25.00, 320.00);
+INSERT INTO Detalle_Factura (id_factura, id_producto, cantidad, total) VALUES
+(3, 3, 20.80, 150.75);
+INSERT INTO Detalle_Factura (id_factura, id_producto, cantidad, total) VALUES
 (4, 4, 51.28, 500.00);
